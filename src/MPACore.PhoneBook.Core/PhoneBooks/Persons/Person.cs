21108c4f1c77eee8866d9778b99489112a2d9ac6
qxinzheng <312782144@qq.com>
2019-07-14 17:15:12 +0800

    新建Person和PhoneNumber实体

---
+++ src/MPACore.PhoneBook.Core/PhoneBooks/Persons/Person.cs
@@ -0,0 +1,33 @@
+﻿using Abp.Domain.Entities;
+using Abp.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MPACore.PhoneBook.PhoneBooks.Persons
+{
+    /// <summary>
+    /// 人员
+    /// </summary>
+    public class Person : FullAuditedEntity
+    {
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        [Required]
+        [MaxLength(50)]
+        public string Name { set; get; }
+        /// <summary>
+        /// 邮箱地址
+        /// </summary>
+        [EmailAddress]
+        [MaxLength(80)]
+        public string EmailAddress { set; get; }
+        /// <summary>
+        /// 地址信息
+        /// </summary>
+        [MaxLength(200)]
+        public string Address { set; get; }
+    }
+}
--- src/MPACore.PhoneBook.EntityFrameworkCore/EntityFrameworkCore/PhoneBookDbContext.cs
+++ src/MPACore.PhoneBook.EntityFrameworkCore/EntityFrameworkCore/PhoneBookDbContext.cs
@@ -3,6 +3,8 @@ using Abp.Zero.EntityFrameworkCore;
 using MPACore.PhoneBook.Authorization.Roles;
 using MPACore.PhoneBook.Authorization.Users;
 using MPACore.PhoneBook.MultiTenancy;
+using MPACore.PhoneBook.PhoneBooks.Persons;
+using MPACore.PhoneBook.PhoneBooks.PhoneNumbers;
 
 namespace MPACore.PhoneBook.EntityFrameworkCore
 {
@@ -13,6 +15,16 @@ namespace MPACore.PhoneBook.EntityFrameworkCore
         public PhoneBookDbContext(DbContextOptions<PhoneBookDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<PhoneNumber> PhoneNumbers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>().ToTable("Person","PB");
+            modelBuilder.Entity<PhoneNumber>().ToTable("PhoneNumber", "PB");
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
